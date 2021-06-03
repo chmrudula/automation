7657606cff0f90911aaf29189fa21b8191cb75b4
chmrudula <chmrudula14@gmail.com>
2021-06-03 22:48:42 +0200

    Add volvo website and test the title is present

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,7 +1,7 @@
 describe('experitest.com page', () => { 
     it('should have the right title', () => { 
-        browser.url('https://experitest.com.') 
+        browser.url('https://www.volvocars.com/intl/v/car-safety/a-million-more') 
         const title = browser.getTitle() 
-        expect(browser).toHaveTitle('Experitest: Mobile App & Cross-Browser Testing End-to-End'); 
+        expect(browser).toHaveTitle('volvo'); 
     }) 
 })
