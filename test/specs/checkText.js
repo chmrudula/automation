4918ecf53d25353be5cea70745c1868369d3b410
chmrudula <chmrudula14@gmail.com>
2021-06-03 23:26:34 +0200

    Added test cases for title, cars name, heading1

--- test/specs/checkText.js
+++ test/specs/checkText.js
@@ -1,7 +1,20 @@
-describe('experitest.com page', () => { 
+describe('volvocars.com page', () => {
+
     it('should have the right title', () => { 
         browser.url('https://www.volvocars.com/intl/v/car-safety/a-million-more') 
         const title = browser.getTitle() 
         expect(browser).toHaveTitle('volvo'); 
     }) 
+
+    it('should have the right name Cars', () => { 
+        browser.url('https://www.volvocars.com/intl/v/car-safety/a-million-more') 
+        const elem = $('#Cars')
+        expect(elem).toExist()
+    })
+
+    it('should have the right heading1', () => { 
+        browser.url('https://www.volvocars.com/intl/v/car-safety/a-million-more') 
+        const elem = $('#Ideas that change the world are often the most controversial.')
+        expect(elem).toExist()
+    })
 })
